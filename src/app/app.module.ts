c4d6af7929386da800c677dc61a776fe7ab901ab
Brindha2324 <bean41768@gmail.com>
2024-07-04 14:53:46 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InternshipsComponent } from './internships/internships.component';
+import { ResponsiveDirective } from './directives/responsive.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    ContactComponent,
+    EducationComponent,
+    PersonalInformationComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    InternshipsComponent,
+    ResponsiveDirective,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/internships/internships.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Internship } from '../models/models';
+
+@Component({
+  selector: 'app-internships',
+  templateUrl: './internships.component.html',
+  styleUrls: ['./internships.component.css']
+})
+export class InternshipsComponent implements OnInit {
+  workExpList: Internship[] = [
+    {
+      role: 'Chief Executing Officer',
+      company: 'Google (Alphabet)',
+      duration: 'Aug 2015 - Now',
+      description: [
+        'Working to expand google in remote areas.',
+        'Working to develop new technologies to make life easier.',
+      ],
+    },
+    {
+      role: 'Senior Vice President',
+      company: 'Google',
+      duration: 'Apr 2012 - Aug 2015',
+      description: [
+        'Involved in various Public talks',
+        'Open Sourced Google VP8',
+      ],
+    },
+    {
+      role: 'Vice President',
+      company: 'Google',
+      duration: 'Jul 2008 - Mar 2012',
+      description: [
+        'Included Android in Google',
+        'Worked on Development of Google Maps',
+      ],
+    },
+    {
+      role: 'Product Manager',
+      company: 'Google',
+      duration: 'Apr 2004 - Jun 2008',
+      description: [
+        'Worked on Google Toolbar',
+        'Involved in team to develop Google Chrome',
+      ],
+    },
+    {
+      role: 'Software Developer',
+      company: 'McKinsey & Co',
+      duration: 'Sep 2002 - Mar 2004',
+      description: [
+        'Worked with multiple teams to develop desktop and web applications',
+        'Worked on different technologies such as (Dotnet, C++, Java)',
+      ],
+    },
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
